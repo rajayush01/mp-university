5fb110ef1c3382588ebd0009a47046b39f692251
rajayush01 <119657235+rajayush01@users.noreply.github.com>
2026-02-23 20:25:02 +0530

    loading changes

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import dpslogo from "../../assets/logo_main.png";
+import dpslogo from "../../assets/logo_main.jpg";
 
 
 type SizeType = 'sm' | 'md' | 'lg';
